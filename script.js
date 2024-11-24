9d96250998d50f03aaf32d20a8f8f584b1161221
Ujjawal Kumar <ujjawalraj131@gmail.com>
2024-11-24 22:41:23 +0530

    Initial commit

--- script.js
+++ script.js
@@ -8,6 +8,13 @@
 // 	document.getElementById("myBar").style.width = scrolled + "%";
 // }
 
+
+
+
+
+
+
+
 function scrollAppear() {
   var introText = document.querySelector(".side-text");
   var sideImage = document.querySelector(".sideImage");
